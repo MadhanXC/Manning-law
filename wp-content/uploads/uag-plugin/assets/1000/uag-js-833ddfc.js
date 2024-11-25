133a591624bb21c2f2ba2865155565c3f9536cab
MadhanRajRK <madhanraj791@gmail.com>
2024-11-25 11:00:29 -0600

    Updates

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-833ddfc.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-96f5d7b0' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-59c410a7' );
+});
+ });
